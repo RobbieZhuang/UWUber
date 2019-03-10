b7f35ea707eb5cf0d9d2fdc8ae894750d7f1797a
Bimesh De Silva <bimesh.de.silva@sap.com>
2019-03-10 19:33:57 -0400

    Added go lambda function that can be triggered by SNS

---
+++ Parsing/add_post/add_post.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"log"
+	"time"
+)
+
+const tableName string = "ride_posts"
+const awsRegion string = "us-east-1"
+
+var svc *dynamodb.DynamoDB
+
+type ResponseBody struct {
+	Response string `json:"response"`
+}
+
+type Post struct {
+	Message string `json:"message"`
+	PostTime string `json:"postTime"`
+}
+
+type PostEntity struct {
+	PostId string `json:"id"`
+	Message string `json:"message"`
+	PostTime string `json:"postTime"`
+}
+
+func saveToDatabase(job PostEntity) {
+	dbMap, err := dynamodbattribute.MarshalMap(job)
+	if err != nil {
+		log.Println("Got error marshalling job entity:")
+		log.Println(err.Error())
+	}
+
+	dbInput := &dynamodb.PutItemInput{
+		Item: dbMap,
+		TableName: aws.String(tableName),
+	}
+
+	out, err := svc.PutItem(dbInput)
+
+	if err != nil {
+		log.Println("Error calling put item ", err.Error())
+	} else {
+		log.Println("Successfully added ", job, " to table ", tableName)
+		log.Println("Database response: ", out)
+	}
+}
+
+// TODO: check if post already exists and return error
+func handle(ctx context.Context, snsEvent events.SNSEvent) (events.APIGatewayProxyResponse, error) {
+	log.Println("Post: ", snsEvent)
+	log.Println("context ", ctx)
+	headers := map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+
+	var post Post
+	jsonParseError := json.Unmarshal([]byte(snsEvent.Records[0].SNS.Message), &post)
+	if jsonParseError != nil {
+		log.Println(jsonParseError)
+		return events.APIGatewayProxyResponse{500, headers, nil, "Internal Server Error", false}, nil
+	}
+
+	log.Println("Post received ", post)
+	code := 200
+	response, jsonBuildError := json.Marshal(ResponseBody{Response: "Added " + post.Message + " to database"})
+	if jsonBuildError != nil {
+		log.Println(jsonBuildError)
+		response = []byte("Internal Server Error")
+		code = 500
+	}
+
+	saveToDatabase(PostEntity{string(time.Now().UnixNano() * int64(time.Nanosecond) / int64(time.Millisecond)), post.Message, post.PostTime})
+
+	return events.APIGatewayProxyResponse{code, headers, nil, string(response), false}, nil
+}
+
+func main() {
+	session, err := session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion)},
+	)
+
+	// Create DynamoDB client
+	svc = dynamodb.New(session)
+
+	if err != nil {
+		log.Println("Error initiating dynamodb for get_fb_group_posts lambda function ", err.Error())
+	} else {
+		log.Println("Successfully initiated dynamodb for get_fb_group_posts lambda function")
+		lambda.Start(handle)
+	}
+}
